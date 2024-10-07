eb6b872319b0d3886d9cfcdcdc2b8afb8350d4b4
Ron Sims II <ronsims2@gmail.com>
2024-10-07 19:15:40 -0400

    Adjusting API of button to see if I could fix ref error

--- packages/fk-react-vanillae/src/button/button.tsx
+++ packages/fk-react-vanillae/src/button/button.tsx
@@ -2,16 +2,14 @@ import React, { forwardRef } from 'react'
 import { camelCase } from 'lodash-es'
 
 // The camelcase is there just to simulate complexity
-function _Button(props, ref) {
+export const Button = forwardRef(function Button(props, ref) {
   const { onClick, label, children } = props
   return (
     <button ref={ref} onClick={onClick}>
       {children ? children : camelCase(label)}
     </button>
   )
-}
-
-export const Button = forwardRef(_Button)
+})
 
 export enum ButtonSizes {
   xsmall,
