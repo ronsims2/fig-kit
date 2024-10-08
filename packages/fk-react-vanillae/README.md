bf2373cc5878fe0a012a0b0558a42ec285ef0bf1
Ron Sims II <ronsims2@gmail.com>
2024-10-08 18:31:51 -0400

    Added a simple css example and updated docs on how to add the fiel to a Vite project

---
+++ packages/fk-react-vanillae/README.md
@@ -0,0 +1,14 @@
+## Installation
+
+Install the package:
+
+`npm install @orangemantis/fk-react-vanillae@latest`
+
+## Setup
+
+### CSS 
+
+You can import the the stylesheet form the `node_modules` directory:
+
+`@import url('@orangemantis/fk-react-vanillae/dist/style.css');
+`
--- packages/fk-react-vanillae/src/button/button.tsx
+++ packages/fk-react-vanillae/src/button/button.tsx
@@ -1,11 +1,13 @@
 import React, { forwardRef } from 'react'
 import { camelCase } from 'lodash-es'
 
+import './button.css'
+
 // The camelcase is there just to simulate complexity
 export const Button = forwardRef(function Button(props, ref) {
-  const { onClick, label, children } = props
+  const { onClick, label, children, className } = props
   return (
-    <button ref={ref} onClick={onClick}>
+    <button className={`fk-btn ${className}`} ref={ref} onClick={onClick}>
       {children ? children : camelCase(label)}
     </button>
   )
