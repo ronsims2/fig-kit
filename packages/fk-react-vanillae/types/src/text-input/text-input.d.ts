8039f19e6f040b9d1a18f85afff28093656da6f5
Ron Sims II <ronsims2@gmail.com>
2024-10-10 22:46:28 -0400

    Working on getting imports to work when added to a vite project

---
+++ packages/fk-react-vanillae/types/src/text-input/text-input.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import './text-input.css';
+export type TextInputProps = {
+    placeholder: string;
+    defaultValue: string;
+    name: string;
+    className: string;
+};
+export declare const TextInput: React.ForwardRefExoticComponent<TextInputProps & React.RefAttributes<HTMLInputElement>>;
+//# sourceMappingURL=text-input.d.ts.map
