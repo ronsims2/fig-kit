529b59715b58abdb734a7eaa5f4cc0a737b8e503
Ron Sims II <ronsims2@gmail.com>
2025-01-11 15:27:01 -0500

    Updated label API

--- packages/fk-react-vanillae/src/label/label.tsx
+++ packages/fk-react-vanillae/src/label/label.tsx
@@ -3,14 +3,16 @@ import './label.css'
 export type LabelProps = {
   text: string
   className?: string | string[]
+  children?: string
 }
+
 export function Label(props: LabelProps) {
-  const { text, className } = props
+  const { text, className, children } = props
 
   //@ts-expect-error TS2339
   const styles = className?.push
     ? //@ts-expect-error TS2339
       className.push('fk-label')
     : (className + ' fk-label').trim()
-  return <div className={styles}>{text}</div>
+  return <div className={styles}>{children ?? text}</div>
 }
