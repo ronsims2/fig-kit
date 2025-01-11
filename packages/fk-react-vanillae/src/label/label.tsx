57864ec62f3f35594e3f000ef92581feef7583df
Ron Sims II <ronsims2@gmail.com>
2025-01-11 14:38:04 -0500

    Supressed TS garbage errors

--- packages/fk-react-vanillae/src/label/label.tsx
+++ packages/fk-react-vanillae/src/label/label.tsx
@@ -7,8 +7,10 @@ export type LabelProps = {
 export function Label(props: LabelProps) {
   const { text, className } = props
 
+  //@ts-expect-error TS2339
   const styles = className?.push
-    ? className.push('fk-label')
+    ? //@ts-expect-error TS2339
+      className.push('fk-label')
     : (className + ' fk-label').trim()
   return <div className={styles}>{text}</div>
 }
--- packages/fk-react-vanillae/types/src/label/label.d.ts
+++ packages/fk-react-vanillae/types/src/label/label.d.ts
@@ -3,5 +3,5 @@ export type LabelProps = {
     text: string;
     className?: string | string[];
 };
-export declare function Label(props: labelProps): import("react/jsx-runtime").JSX.Element;
+export declare function Label(props: LabelProps): import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=label.d.ts.map
